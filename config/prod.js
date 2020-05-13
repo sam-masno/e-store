71202108c44b325032a2e2b3766b5c607adf9af4
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2020-05-13 14:00:13 +0500

    fixed heroku config

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,6 @@ module.exports = {
     jwtSecret: process.env.JWT_SECRET,
     BTmerchantId: process.env.BT_MERCHANT_ID,
     BTprivateKey: process.env.BT_PRIVATE_KEY,
-    BTpublicKey: process.env.BT_PUBLIC_KEY
+    BTpublicKey: process.env.BT_PUBLIC_KEY,
+    bucket: process.env.AWS_BUCKET
 }
